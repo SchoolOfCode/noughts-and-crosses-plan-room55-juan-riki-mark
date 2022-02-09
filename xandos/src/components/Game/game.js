8470f0180285e3556af85581a461ece9a0710759
Rikiah <rik_28@hotmail.co.uk>
2022-02-09 14:43:28 +0000

    img removed

--- xandos/src/components/Game/game.js
+++ xandos/src/components/Game/game.js
@@ -1,11 +1,10 @@
-import "./game.css";
+import "../Game/game.module.css";
 import Board from "../Board/board";
 
 function Game() {
   return (
     <div className="Game">
       <header className="Game-header">Humans vs. Robots</header>
-      <div className={Style.bkgdimage}></div>
       <Board />
     </div>
   );
